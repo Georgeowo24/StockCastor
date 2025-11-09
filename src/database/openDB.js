93b64b21bc17fca3a05f371a1ced29450bc8bc6a
Jorge Torres Romero <jtorresromero30@gmail.com>
2025-11-08 20:30:36 -0600

    Corrigiendo el error de la BD

--- src/database/openDB.js
+++ src/database/openDB.js
@@ -1,8 +1,15 @@
 import * as SQLite from 'expo-sqlite';
 
+let db = null;
+
 //? Función para abrir o crear la base de datos
 export async function openDatabase() {
-    const db = await SQLite.openDatabaseAsync('StockCastor.db');
+    if ( db ) {
+        console.log("Base de datos reabierta exitosamente");
+        return db;
+    }
+    
+    db = await SQLite.openDatabaseAsync('StockCastor.db');
     console.log("Base de datos abierta exitosamente");
     return db;
 }
